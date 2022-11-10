f59384d831b094783bcac5eec57916202f897a34
washtakzo <washtakzo@gmail.com>
2022-11-10 16:40:34 +0100

    lunch trailer search on animeCard click

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -16,7 +16,13 @@ const cutText = (text: string, size: number) => {
 
 const AnimeCard = ({ anime }: Props) => {
   return (
-    <div className="">
+    <div
+      onClick={() => {
+        window.open(
+          `https://www.youtube.com/results?search_query=${anime.title}+trailer`
+        );
+      }}
+    >
       <div className=" relative h-[280px] sm:h-[360px] w-44 sm:w-52 object-fill m-auto hover:scale-105 transition duration-300 hover:cursor-pointer">
         <p className="text-transparent text-center  z-10 text-sm sm:text-md lg:text-[1rem] sm:font-semibold hover:text-white hover:bg-[#0d1c2394]  py-2 absolute top-0 bottom-0 left-[-0px] right-[-0px]">
           {cutText(anime?.description, 300)}
